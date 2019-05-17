a4f2e96bdb39cde68cf48530453d3188677d7bd7
Korte-PC\Korte <korte.stavros@hotmail.com>
2019-05-18 01:49:36 +0300

    add clear functionality

--- src/app/submitted-class.directive.ts
+++ src/app/submitted-class.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef, Input, OnChanges, OnDestroy, SimpleChanges } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { fromEvent, Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, filter, subscribeOn } from 'rxjs/operators';
 
 @Directive({
   selector: '[appSubmittedClass]'
@@ -11,7 +11,9 @@ export class SubmittedClassDirective implements OnChanges, OnDestroy {
 
   @Input() formGroup: FormGroup;
 
-  private destroy$ = new Subject();
+  private removeClassDestroy$ = new Subject();
+
+  private addClassDestroy$ = new Subject();
 
   private defaultClassName = 'submitted';
 
@@ -20,29 +22,48 @@ export class SubmittedClassDirective implements OnChanges, OnDestroy {
   ngOnChanges(changes: SimpleChanges) {
     if (changes.formGroup && changes.formGroup.currentValue) {
       this.addClassOnSubmission();
+      this.removeClassOnClear();
     }
   }
 
   ngOnDestroy() {
-    this.destroy$.next();
-    this.destroy$.unsubscribe();
+    [this.addClassDestroy$, this.removeClassDestroy$].forEach(subscription => {
+      subscription.next();
+      subscription.complete();
+    });
   }
 
   private addClassOnSubmission() {
     /**Remove any previous subscriptions */
-    this.destroy$.next();
+    this.addClassDestroy$.next();
 
     fromEvent(this.formElement, 'submit')
-      .pipe(takeUntil(this.destroy$))
+      .pipe(takeUntil(this.addClassDestroy$))
       .subscribe(() => {
-        this.formElement.classList.add(this.appSubmittedClass || this.defaultClassName);
+        this.formElement.classList.add(this.submittedClass);
+        this.removeClassOnClear();
+        this.addClassDestroy$.next();
+      });
+  }
 
-        /**Remove subscription once class is added */
-        this.destroy$.next();
+  private removeClassOnClear() {
+    this.formGroup.valueChanges
+      .pipe(
+        filter(newVal => !Object.values(newVal).find(value => value !== null)),
+        takeUntil(this.removeClassDestroy$)
+      )
+      .subscribe(() => {
+        this.formElement.classList.remove(this.submittedClass);
+        this.removeClassDestroy$.next();
+        this.addClassOnSubmission();
       });
   }
 
   private get formElement(): HTMLFormElement {
     return this.el.nativeElement;
+  }
+
+  private get submittedClass(): string {
+    return this.appSubmittedClass || this.defaultClassName;
   }
 }
